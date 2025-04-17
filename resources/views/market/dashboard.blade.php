2f5d605262db8b2cfdc46590946048205067c07e
Douwes Dekker <dopokan@gmail.com>
2025-04-17 15:08:21 +0700

    add report update typography

--- resources/views/market/dashboard.blade.php
+++ resources/views/market/dashboard.blade.php
@@ -50,7 +50,11 @@
                         <h6 class="text-capitalize">
                             Report Jumlah Usaha Berdasarkan Kabupaten/Kota
                         </h6>
-                        <p class="text-sm">Kondisi tanggal: {{ $updateDate }} {{ $updateTime }}</p>
+                        <p class="text-sm mb-1"><strong>Report tidak realtime</strong>, report akan diupdate pada jam
+                            <strong>06.00, 12.00, 18.00,
+                                22.30</strong>
+                        </p>
+                        <p class="text-sm">Kondisi tanggal: <strong>{{ $updateDate }} {{ $updateTime }}</strong></p>
                     </div>
                     <div class="card-body">
                         <div style="width: 75%; margin: auto;">
