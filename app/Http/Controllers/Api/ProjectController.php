5421cd09fd0349d45ad708b606f79e621376d2b1
Douwes Dekker <dopokan1@gmail.com>
2025-06-17 00:15:45 +0700

    add user to project

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -22,7 +22,7 @@ class ProjectController extends Controller
 
     public function getProjectsByUser($user)
     {
-        $projects = Project::where('user_id', $user)->get();
+        $projects = Project::with(['user', 'user.organization'])->where('user_id', $user)->get();
         return $this->successResponse($projects);
     }
 
