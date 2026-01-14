7cd58ab9d34b52ad141a2e6a764c2992802a8572
zipo <dopokan@gmail.com>
2026-01-14 03:02:45 +0000

    modify docker and close API KDM

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            'api-closed' => \App\Http\Middleware\CloseSpecificApi::class,
         ]);
         if (env('APP_ENV') === 'production') {
             $middleware->trustProxies(at: '*');
